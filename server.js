5bf1c7455a300c3091ff8d1b5a38e456d743df1f
CS <chadi.samadi96@gmail.com>
2024-07-08 01:06:48 +0300

    V1

---
+++ server.js
@@ -0,0 +1,22 @@
+// const express = require('express')
+// var cors = require('cors')
+// const app = express()
+// app.use(cors())
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+
+// app.listen(3000)
+
+const express = require("express");
+var cors = require('cors')
+const app = express();
+const PORT = process.env.PORT || 3030;
+app.use(cors());
+
+// your code
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
